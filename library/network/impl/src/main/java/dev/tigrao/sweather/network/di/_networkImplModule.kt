85a236f0e7a6c1df8ea5648a85bc217eecd8d7df
Gabriel Costa <gabrielcosta08@gmail.com>
2021-04-21 15:26:12 -0300

    Add network modules keys and url

--- library/network/impl/src/main/java/dev/tigrao/sweather/network/di/_networkImplModule.kt
+++ library/network/impl/src/main/java/dev/tigrao/sweather/network/di/_networkImplModule.kt
@@ -1,7 +1,9 @@
 package dev.tigrao.sweather.network.di
 
-import br.com.hippopotamus.tabarato.network.BuildConfig
+import dev.tigrao.sweather.network.BuildConfig
+import dev.tigrao.sweather.network.NetworkBuilder
 import dev.tigrao.sweather.network.http.retry.RetryCallAdapterFactory
+import dev.tigrao.sweather.network.interceptor.ApiKeyInterceptor
 import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.bind
@@ -9,6 +11,12 @@ import org.koin.dsl.module
 import retrofit2.CallAdapter
 
 val networkImplModule = module {
+
+    single {
+        NetworkBuilder(
+            baseUrl = BuildConfig.API_URL,
+        )
+    }
 
     factory {
         HttpLoggingInterceptor()
@@ -21,4 +29,6 @@ val networkImplModule = module {
     factory {
         RetryCallAdapterFactory()
     } bind CallAdapter.Factory::class
+
+    factory<Interceptor> { ApiKeyInterceptor() } bind Interceptor::class
 }
