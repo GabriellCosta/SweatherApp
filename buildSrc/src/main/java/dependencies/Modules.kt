c40f4a4b0f5a11c80ca2554662c747a517e2710f
Gabriel Costa <gabrielcosta08@gmail.com>
2021-04-21 16:32:36 -0300

    Add weather-view module entry

--- buildSrc/src/main/java/dependencies/Modules.kt
+++ buildSrc/src/main/java/dependencies/Modules.kt
@@ -5,7 +5,10 @@ object Modules {
     const val main = ":main"
 
     object Features {
-
+        object WeatherView {
+            const val public = ":feature:weather-view:public"
+            const val impl = ":feature:weather-view:impl"
+        }
     }
 
     object Library {
