ac9af23c4171a63f985c78491513249eb287cf60
Gabriel Costa <gabrielcosta08@gmail.com>
2021-04-28 11:41:41 -0300

    Add retry for fetch api data

--- feature/weather-view/impl/src/main/kotlin/dev/tigrao/sweather/weather/view/data/WeatherViewApi.kt
+++ feature/weather-view/impl/src/main/kotlin/dev/tigrao/sweather/weather/view/data/WeatherViewApi.kt
@@ -1,5 +1,6 @@
 package dev.tigrao.sweather.weather.view.data
 
+import dev.tigrao.sweather.network.http.Retry
 import dev.tigrao.sweather.weather.view.data.response.GeolocationResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,6 +8,7 @@ import retrofit2.http.Query
 internal interface WeatherViewApi {
 
     @GET("weather?units=metric")
+    @Retry
     suspend fun getDataByGeographicCoordinates(
         @Query("lat") lat: Double,
         @Query("lon") lon: Double
